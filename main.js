c5c873ff1fa2bc1552523cfc3284d088b3070c8f
Nicholas Eduardo <contato.nicholasfdl@gmail.com>
2023-09-02 19:01:29 -0300

    pequenas atualizações
    Nesse momento, já consegui criar algo usando o html. Nas próximas atualizações eu tentarei automatizar alguns processos usando o JS, e deixar o site mais bonito usando o CSS.

---
+++ main.js
@@ -0,0 +1,11 @@
+//const nomeLivro = "Cinder";
+//const genero = "fantasia";
+//const idade = "+14";
+//const nomeArqImg = "cinder.jpeg";
+
+const livro1 = {
+    nome: "Cinder",
+    genero: "Fantasia",
+    idaide: "+14",
+    nomeArqImg: "cinder.jpeg",
+};
